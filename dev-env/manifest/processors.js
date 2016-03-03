762a0f1bf59479f6acd4331520f57b61784c64da
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-03-03 14:32:22 +0700

    init project

---
+++ dev-env/manifest/processors.js
@@ -0,0 +1,32 @@
+import Csp         from './processor/csp'
+import PackageJson from './processor/package_json'
+import Assets      from './processor/assets'
+import Action      from './processor/action'
+import Background  from './processor/background'
+import Content     from './processor/content'
+import Overrides   from './processor/overrides'
+import Locales   from './processor/locales'
+import Options   from './processor/options'
+
+const processors = [
+    // Fix csp for devel
+    Csp,
+    // Mege package.json
+    PackageJson,
+    // Process assets
+    Assets,
+    // Process action (browse, or page)
+    Action,
+    // Process background script
+    Background,
+    // Process content script
+    Content,
+    // Process overrides
+    Overrides,
+    // Process locales
+    Locales,
+    // Process options page
+    Options
+]
+
+export default processors
